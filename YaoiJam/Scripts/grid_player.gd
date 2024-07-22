a6eba508a436f4f1e75732a454b375ff1785e0f5
SrDoppelganger <doppelganger.dev2004@gmail.com>
2024-07-22 14:37:38 -0300

    Added basic grid movement

--- YaoiJam/Scripts/grid_player.gd
+++ YaoiJam/Scripts/grid_player.gd
@@ -14,9 +14,23 @@ func _physics_process(delta):
 
 func move(direction:Vector2):
 	# pegar tile atual
-	var currTile:Vector2i = tilemap.local_to_map() 
+	var currTile:Vector2i = tilemap.local_to_map(global_position) 
 	
 	#pegar o tile-alvo (proximo tile)
+	#combina a posição do tile atual com a do tile-alvo para mover o jogador
+	var targetTile:Vector2i = Vector2i(
+		currTile.x + direction.x,
+		currTile.y + direction.y
+	)
 	
+	print(currTile)
+	print(targetTile)
 	
 	#verificar se o jogador pode andar no tile-alvo
+	var tileData:TileData = tilemap.get_cell_tile_data(0, targetTile)
+	
+	if tileData.get_custom_data("Walkable") == false:
+		return
+		
+	#move o jogador
+	global_position = tilemap.map_to_local(targetTile)
